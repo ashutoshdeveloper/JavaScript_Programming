073e87a25cc2d4f54e3d9c62c0d511a952ca134c
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-18 09:54:14 +0530

    write a program to create a word -meanig dictonary of 3 words

---
+++ Variable and data/Q1.5.js
@@ -0,0 +1,10 @@
+//Try to add a new key to the previous code
+const obj={
+    name:"ashu",
+    surname:"nanda",
+    profession:"student"
+}
+obj.college="IGIT"
+obj.rollno=12
+
+console.log(obj)
---
+++ Variable and data/Q1.6.js
@@ -0,0 +1,10 @@
+// write a program to create a word -meanig dictonary of 3 words
+
+let word={
+    proclaim:"to make something known officially or publicly",
+    hassle:"a thing or situation that is annoying because it is complicated or involves a lot of effort",
+    impoverish:"to make somebody/something poor or lower in quality"
+
+}
+
+console.log(word)
