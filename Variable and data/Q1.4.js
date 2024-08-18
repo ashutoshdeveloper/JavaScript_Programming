c9b3dec19e61756873f8fb4e3b101073f35d5798
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-18 09:53:18 +0530

    create a const object in javascript.Can we change it to hold a number later?

---
+++ Variable and data/Q1.4.js
@@ -0,0 +1,8 @@
+//create a const object in javascript.Can we change it to hold a number later?(ans-No)
+const obj={
+    name:"ashu",
+    surname:"nanda",
+    profession:"student"
+}
+
+console.log(obj)
