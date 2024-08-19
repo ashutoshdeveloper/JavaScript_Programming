22085361e617a2764e15bb10453d83b96add57d4
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-19 11:10:18 +0530

    q-1

---
+++ Expression and conditional/Q1.1.js
@@ -0,0 +1,9 @@
+// Use logical operator to find whether the age of a person lies in between 10 and 20
+let age= 19;
+if(10<age && age<20)
+{
+    console.log("The person age is lies between 10 and 20")
+
+}else{
+    console.log("The person age does not lies between 10 and 20")
+}
