56205a6cb88c22fe7b40de31a8995093324e0ea5
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-19 11:23:19 +0530

    q-3

---
+++ Expression and conditional/Q1.3.js
@@ -0,0 +1,9 @@
+// write a program to find whether a number is divisible by 2 and 3
+let num=96;
+if(num %2==0 && num % 3==0)
+{
+    console.log(`${num} is divisible by 2 and 3 both`)
+}
+else{
+    console.log(`${num} is not divisible by 2 and 3`)
+}
