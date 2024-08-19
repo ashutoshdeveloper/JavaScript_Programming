75c3f2832f97875230a6afb8f3a771a21643f4fd
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-19 11:10:56 +0530

    q-2

---
+++ Expression and conditional/Q1.2.js
@@ -0,0 +1,24 @@
+// DEmonstrate the use of switch case in JS
+let student = "Manish";
+switch(student){
+    case "Ram":
+        console.log("The student name is Ram")
+        break;
+    case "Rahul":
+        console.log("The student name is Rahul")    
+        break;
+    case "Ajay":
+        console.log("The student name is Ajay")  
+        break; 
+    case "Manish":
+        console.log("The student name is Manish")  
+        break; 
+    case "Apurv":
+        console.log("The student name is Apurv")  
+        break; 
+
+    default:
+        console.log("The student name is not found")   
+        break; 
+    
+}
