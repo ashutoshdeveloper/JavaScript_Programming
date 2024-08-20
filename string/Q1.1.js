31ab04a9e346dd26602debd7446bccb378e27883
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-20 23:00:27 +0530

    1

---
+++ string/Q1.1.js
@@ -0,0 +1,5 @@
+// what will console.log("abc\"".length) print ?
+
+console.log("abc\"".length)
+
+// ESCAPE SEQUENCE:= anthing which we can't print inside the console we can write them after '\' and \ is not consider as a part of the console
