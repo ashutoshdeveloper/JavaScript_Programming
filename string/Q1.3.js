111599da18ad33e031807e9f831e2c3ee424ec81
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-20 23:01:20 +0530

    3

---
+++ string/Q1.3.js
@@ -0,0 +1,11 @@
+// Explore startWith and EndWith function in a String
+let str1="Ashutosh Ratna Nanda"
+let b1 = str1.startsWith('My')
+console.log(b1)
+
+// start with returns true when the start string matches with the parameter and if does not matches it return false
+
+// EndWith 
+let str2="Ashutosh Ratna Nanda"
+let b2 = str2.endsWith('Nanda')
+console.log(b2)
