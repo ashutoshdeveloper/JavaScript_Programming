a62b683595694728e1e9b8ef9dd3330855620cfc
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-20 23:00:59 +0530

    2

---
+++ string/Q1.2.js
@@ -0,0 +1,13 @@
+// Explore the includes function of a string
+
+let str1="Hii, My name is Ashutosh"
+let word= "ashutosh"
+
+if(str1.includes(word))
+{
+    console.log("Word found")
+}else{
+    console.log("word missing")
+}
+
+// .include() returns true if the word is present inside the string if not returns false
