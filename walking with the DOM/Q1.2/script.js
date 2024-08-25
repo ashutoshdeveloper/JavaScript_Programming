209a88c91faf1d75b503f52851c381e8a1697746
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-25 16:08:05 +0530

    2

---
+++ walking with the DOM/Q1.2/script.js
@@ -0,0 +1,5 @@
+// Create element of 3 childern and change the color of first and last children
+let a= document.getElementsByTagName("ul")[0]
+
+a.firstElementChild.style.color = "red"
+a.lastElementChild.style.color = "green"
