afab13dd72049980419a03d980311d5590f80924
Ashutosh Ratna Nanda <ashutoshnanda014@gmail.com>
2024-08-25 16:06:08 +0530

    1

---
+++ walking with the DOM/Q.1.1/script.js
@@ -0,0 +1,6 @@
+// create a nav bar and change its first element color to red
+
+let firstElm = document.getElementsByTagName("ul")[0]
+
+console.log(firstElm)
+firstElm.firstElementChild.style.color= "red"
